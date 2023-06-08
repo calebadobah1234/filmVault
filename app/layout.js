abd740b726de9385e5606f3c2bc1666da6100b98
calebadobah1234 <59422459+calebadobah1234@users.noreply.github.com>
2023-06-08 22:41:17 +0000

    aanal

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Roboto } from "next/font/google";
 import GoogleAnalytics from "./components/GoogleAnalytics";
+import Script from "next/script";
 
 const font = Roboto({ subsets: ["latin"], weight: ["400"] });
 
@@ -15,7 +16,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={font.className}>
-      <GoogleAnalytics GA_MEASUREMENT_ID="G-XRQ23BL3CB" />
+      {/* <GoogleAnalytics GA_MEASUREMENT_ID="G-XRQ23BL3CB" /> */}
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-XRQ23BL3CB"
+        ></Script>
+        <Script id="anal2" strategy="beforeInteractive">
+          {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XRQ23BL3CB');`}
+        </Script>
+      </head>
       <body>
         <Navbar />
         {children}
