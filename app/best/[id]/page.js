45b190fba1cae3c13e0a14ace5de2fa7c9f8e13c
calebadobah1234 <59422459+calebadobah1234@users.noreply.github.com>
2023-06-18 23:01:43 +0000

    f

--- app/best/[id]/page.js
+++ app/best/[id]/page.js
@@ -1,6 +1,20 @@
 import React from "react";
 import TopItems from "../../components/TopItems";
 
+export const generateMetadata = async ({ params }) => {
+  const res = await fetch(
+    `https://byteread-final.onrender.com/get-item-details/${params.id}`,
+    {
+      next: { revalidate: 60 },
+    }
+  );
+  const data = await res.json();
+  return {
+    title: `${data.title}`,
+    description: `${data.description}`,
+  };
+};
+
 const page = ({ params }) => {
   return (
     <>
