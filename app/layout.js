b3ea8e04fdd2d4f225a103a8c9e131e2fb632a76
calebadobah1234 <59422459+calebadobah1234@users.noreply.github.com>
2024-09-04 00:09:38 +0000

    cc

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Oswald } from "next/font/google";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 const oswald = Oswald({ subsets: ["latin"] });
 
@@ -18,7 +19,20 @@ export default function RootLayout({ children }) {
       {/* <GoogleAnalytics GA_MEASUREMENT_ID="G-XRQ23BL3CB" /> */}
 
       <body className="text-black">
-        <GoogleTagManager gtmId={"G-G5F92NBCCE"} />
+        <head>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-G5F92NBCCE"
+          ></Script>
+          <Script id="google-analytics">
+            {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-G5F92NBCCE');`}
+          </Script>
+        </head>
+        {/* <GoogleTagManager gtmId={"G-G5F92NBCCE"} /> */}
         <Navbar />
         {children}
         <Footer />
