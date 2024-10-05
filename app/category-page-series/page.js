89fd9789f5a6d02d29585d3e2d23a56089f59eb0
calebadobah1234 <59422459+calebadobah1234@users.noreply.github.com>
2024-10-05 22:06:49 +0000

    this is just to fix the stale category data on vercel 4

--- app/category-page-series/page.js
+++ app/category-page-series/page.js
@@ -15,7 +15,7 @@ export async function generateMetadata({ searchParams }) {
 const page = async ({ searchParams }) => {
   const res = await fetch(
     `https://filmvaultbackend.onrender.com/get-category-dataAiom/?category=${searchParams.category}&limit=30&skip=${searchParams.skip}`,
-    { next: { revalidate: 56400 } }
+    { next: { revalidate: 3600 } }
   );
   const data = await res.json();
 
