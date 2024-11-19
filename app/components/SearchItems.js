c65a0f39ef98f3b9a2495111b4e6fafe96dd4ff8
calebadobah1234 <59422459+calebadobah1234@users.noreply.github.com>
2024-11-19 00:41:57 +0000

    better ad formats added main

--- app/components/SearchItems.js
+++ app/components/SearchItems.js
@@ -1,8 +1,5 @@
 import React from "react";
-// import Link from "next/link";
-// import Image from "next/image";
-// import SearchResultWrapper from "./SearchResultWrapper";
-import Script from "next/script";
+
 import LatestItems from "./LatestItems";
 const SearchItems = (props) => {
   return (
@@ -15,13 +12,6 @@ const SearchItems = (props) => {
       ) : (
         <></>
       )} */}
-      <Script
-        src="//pl25046019.profitablecpmrate.com/a2ec5d29f1060455d67da23054ccb38b/invoke.js"
-        data-cfasync="false"
-        async
-        strategy="afterInteractive"
-      />
-      <div id="container-a2ec5d29f1060455d67da23054ccb38b"></div>
       <div className="mt-5 flex justify-center">
         <LatestItems data={props.data} flex={true} itemsToShow={120} />
       </div>
