ee93a2c4edaa3c2445795c8eee4069bccf865d02
calebadobah1234 <59422459+calebadobah1234@users.noreply.github.com>
2024-11-28 10:12:49 +0000

    Update page.js

--- app/series1/[title]/page.js
+++ app/series1/[title]/page.js
@@ -53,7 +53,7 @@ export async function generateMetadata({ params }) {
 
 const page = async ({ params }) => {
   const res = await fetch(
-    `https://filmvaultbackend-4.onrender.com/get-item-detailsAiom/${params.title}`,
+    `https://filmvaultbackend-xpgy.onrender.com/get-item-detailsAiom/${params.title}`,
     {
       revalidate: 86400,
     }
